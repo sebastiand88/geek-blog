3322fb2da4a27c2b4b7cbc05ee59f2fc19284233
sebastiand88 <68199815+sebastiand88@users.noreply.github.com>
2022-01-26 11:59:11 +0000

    minor UI changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-green-600 py-8">
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white transition duration-500 ease hover:text-green-600">
